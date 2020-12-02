0a57d3f2d347e84338feb8e671af52b1caa9301d
Rebecca Ehua <nellyehua@yahoo.com>
2020-12-02 14:11:32 +0100

    xx

--- src/fr/istic/csr/tp/Zone.java
+++ src/fr/istic/csr/tp/Zone.java
@@ -1,4 +1,39 @@
 package fr.istic.csr.tp;
 
 public class Zone {
+    private int x;
+    private int y;
+    private int nb_sardines;
+
+    public Zone(int x,int y,int nb_sardines){
+        this.x=x;
+        this.y=y;
+        this.nb_sardines=nb_sardines;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getNb_sardines() {
+        return nb_sardines;
+    }
+
+    public void setNb_sardines(int nb_sardines) {
+        this.nb_sardines = nb_sardines;
+    }
+
+    public void  accessZoneRequin(){
+
+    }
 }
+
